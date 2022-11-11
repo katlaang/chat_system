120fda497e8ff1b3f03be79063c9ba2ae8d2998e
Zufan Hagos <zufankim@Zufans-MBP.lan>
2022-11-11 11:14:07 -0600

    added routing

--- chatsystem/src/app/app-routing.module.ts
+++ chatsystem/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ChatViewComponent } from './modules/chat/chat-view/chat-view.component';
+import { RegisterChatterComponent } from './modules/chat/register-chatter/register-chatter.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "register",
+    pathMatch: "full",
+    component: RegisterChatterComponent
+  },
+  {
+    path: "chat",
+    pathMatch: "full",
+    component: ChatViewComponent
+  },
+  {
+    path: "",
+    pathMatch: "full",
+    component: AppComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
